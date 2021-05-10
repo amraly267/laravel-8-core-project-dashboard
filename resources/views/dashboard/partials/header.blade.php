50bc216344d80d8b1a4961d7ef10f62bed9b767a
Amr Aly Abdelaty <Aabdelaty@sure.com.sa>
2021-05-11 00:46:28 +0200

    add arabic font

--- resources/views/dashboard/partials/header.blade.php
+++ resources/views/dashboard/partials/header.blade.php
@@ -2,13 +2,24 @@
 <title>{{$title}}</title>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link rel="shortcut icon" href="{{$logo}}" />
+
 <!--begin::Fonts-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 <!--end::Fonts-->
+
+{{-- <link href='https://fonts.googleapis.com/css?family=Cairo' rel='stylesheet'> --}}
+
 <!--begin::Global Stylesheets Bundle(used by all pages)-->
 @if(config('app.locale') == 'ar')
 <link href="{{asset('plugins/dashboard/global/plugins.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('css/dashboard/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+<link href='https://fonts.googleapis.com/css?family=Cairo' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Cairo' rel='stylesheet'>
+<style>
+body {
+    font-family: 'Cairo';font-size: 22px;
+}
+</style>
 @else
 <link href="{{asset('plugins/dashboard/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('css/dashboard/style.bundle.css')}}" rel="stylesheet" type="text/css" />
