c6b1ea33d1d061bcf06d5782289816a68e010e97
Amr Aly Abdelaty <Aabdelaty@sure.com.sa>
2021-06-12 17:46:32 +0200

    add new modules

--- Modules/Home/Http/Controllers/HomeController.php
+++ Modules/Home/Http/Controllers/HomeController.php
@@ -8,15 +8,17 @@ use Illuminate\Routing\Controller;
 use Modules\Apps\Http\Controllers\Dashboard\BaseController;
 use Modules\Country\Entities\Country;
 use Modules\Admin\Entities\Admin;
+use Modules\Page\Entities\Page;
+use Spatie\Permission\Models\Role;
 
 class HomeController extends BaseController
 {
     public function index()
     {
         $totalAdmins = Admin::count();
-        $totalRoles = 123;      //Role::count();
+        $totalRoles = Role::count();
         $totalCountries = Country::count();
-        $totalPages = 123;      //Page::count();
+        $totalPages = Page::count();
         return view('home::'.$this->controllerResource.'index', compact('totalAdmins', 'totalRoles', 'totalCountries', 'totalPages'));
     }
 }
